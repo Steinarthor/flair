ed7b933318386a689b3e40b8ffa7a6145a03dca2
Steinar Þór Oddsson <steinarthorr@gmail.com>
2020-09-22 20:54:29 +0000

    Fixed some color styles

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { RouteComponentProps } from '@reach/router'
 import Header from '../../components/header/header'
+import styles from './home.scss'
 
 const Home: React.FC<RouteComponentProps> = () => {
     const { t } = useTranslation()
     return (
         <>
             <Header />
-            <main>
+            <main className={styles.home}>
                 <h1>{t('Welcome')}</h1>
             </main>
         </>
